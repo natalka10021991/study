83c22111cd763ef135ff75c1e0325212afbca751
Alex Pol <sinbix001@gmail.com>
2021-06-22 13:42:16 +0300

    fix examples

--- lessons/js/2-context/index.js
+++ lessons/js/2-context/index.js
@@ -1,21 +1,21 @@
 class Clock {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    hello(delay) {
-        setTimeout(() => {
-            console.log(`Hello! I am clock with name ${this.name}`);
-        }, delay);
-    }
-    
-    // Without arrow function
-    // hello(delay) {
-    //     const name = this.name;
-    //     setTimeout(function () {
-    //         console.log(`Hello! I am clock with name ${name}`);
-    //     }, delay);
-    // }
+  hello(delay) {
+    setTimeout(() => {
+      console.log(`Hello! I am clock with name ${this.name}`);
+    }, delay);
+  }
+
+  // Without arrow function
+  // hello(delay) {
+  //     const name = this.name;
+  //     setTimeout(function () {
+  //         console.log(`Hello! I am clock with name ${name}`);
+  //     }, delay);
+  // }
 }
 
-(new Clock('AppleWatch')).hello(1000, 'AppleWatch');
+new Clock('AppleWatch').hello(1000);
